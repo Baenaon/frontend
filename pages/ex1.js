5b5611047668fc04cf7b0eb85453832660d59bbb
Kimjimin97 <sja9707@daum.net>
2022-08-15 01:22:10 +0900

    first commit

--- pages/ex1.js
+++
@@ -1,8 +0,0 @@
-export default function Header(){
-    return (
-        <>
-            <h1>깃 멈춰!</h1>
-            <h1>wlals</h1>
-        </>
-    );
-}
---
+++ pages/login.js
@@ -0,0 +1,16 @@
+import styles from "./login.module.css";
+
+function Kakao_oauth() {
+  const CLIENT_ID = "68060ad6d9c586193684d708d0211d18";
+  const REDIRECT_URI = "http://localhost:3000/kakao_oauth";
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+  return (
+    <>
+      <a href={KAKAO_AUTH_URL}>
+        <div className={styles.kakao_btn}></div>
+      </a>
+    </>
+  );
+}
+
+export default Kakao_oauth;
